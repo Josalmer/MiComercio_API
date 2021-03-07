9dc763122e4482ea7f849fff9b78109fb67491d6
Jose Saldaña <jose.saldana@nazaries.com>
2021-03-07 12:44:59 +0100

    Hotfix: solved error when cancelling appointment without app user

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -40,7 +40,7 @@ class Api::V1::AppointmentsController < Api::BaseController
     appointment = Appointment.find(id_permitted['id'])
 
     if appointment.update(removed_at: Time.current)
-      if current_api_v1_user == appointment.company.user
+      if current_api_v1_user == appointment.company.user && appointment.user
         appointment.create_user_notification('user_when_manager_cancel_appointment')
       else
         appointment.create_manager_notification('manager_appointment_cancelled')
