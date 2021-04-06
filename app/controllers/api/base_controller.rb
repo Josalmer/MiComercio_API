1f365370e366e6f80b7c6f0d5c955687027378cc
Jose Saldaña <jose.saldana@nazaries.com>
2021-04-06 19:17:04 +0200

    Added english translations

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,10 @@
 class Api::BaseController < ActionController::API
   before_action :authenticate_api_v1_user!
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.default_locale = request.headers["language"] || :es
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
 
   rescue_from CanCan::AccessDenied do |exception|
     sign_out current_user
     redirect_to new_session_path(User), alert: exception.message
+  end
+
+  private
+
+  def set_locale
+    I18n.default_locale = request.headers["language"] || :es
   end
 end
